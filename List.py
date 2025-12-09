7ca08e157a5548ce6343c1125ce35e5649dd091f
anish9864 <seeyouanish@gmail.com>
2025-12-09 20:46:57 +0545

    list and list comprehension

---
+++ List.py
@@ -0,0 +1,19 @@
+#List (Data Structure)
+# name = ["ram", "shyam", "mohan","rita","sita","Kamal"]
+# # name.append("Shyam") #Adding value at the end of list
+# # name.insert(2,"Hari") #Adding value at specific index
+# # name.pop() #Removing last value from list
+# # name.pop(3) #Removing specific value from list
+# # name.remove("mohan") #Removing specific value from list
+# # x= name.index("mohan") #Getting index of specific data
+# # name.sort() #Sorting the list in ascending order
+# # name.reverse() #Reversing the list
+# name.extend(["koplia","Bipana","Bhawana"]) #adding multiple values at the end of the list
+# print(name)
+
+# ruppe = [1000, 500, 2000, 1500, 3000, 9860, 9999]
+# #ruppe.sort() #Ascending order
+# #ruppe.sort(reverse=True) #Descending order
+# #ruppe.reverse() #Reversing the list
+# print(ruppe)
+
--- whileloop.py
+++ whileloop.py
@@ -31,11 +31,3 @@
 # for i in range(10,0,-1):
 #     print(i)
 
-i = 10
-while i >= 1:
-    print(i)
-    i = i - 1
-    
-    
-for i in range(10,0,-1):
-    print(i)
