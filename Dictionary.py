232eaae1b133f88bdac7fa24a185dfca1933be94
anish9864 <seeyouanish@gmail.com>
2025-12-14 20:36:37 +0545

    Dictionary Complete

--- Dictionary.py
+++ Dictionary.py
@@ -29,3 +29,19 @@
 # car["model"] = 2027
 # car["price"] = 5000000
 # print(car)
+
+
+# 2025/12/14
+# using update() method to change the values of dictionary
+car = {
+    
+    "brand" : "toyota",
+    "color" : ["black","white","red"],
+    "model" : 2025,
+    "price" : 2000000
+    
+}
+# car.update({"brand":"tesla", "model" : 2026, "color" : ["black","white","red","brown", "blue"]})
+car.update({"engine_type" : "electric"})
+
+print(car)
